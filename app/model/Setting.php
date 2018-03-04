316858f0915c2450040c26401d28522671ef0567
Filip Šedivý <mail@filipsedivy.cz>
2018-03-04 23:23:00 +0100

    Uzavření dotazu do transakce

--- app/model/Setting.php
+++ app/model/Setting.php
@@ -44,24 +44,60 @@ class Setting
     }
 
     /**
-     * @param $key   string
-     * @param $value string
+     * @param string      $key
+     * @param string|null $value
+     * @param string|null $group
+     * @param string|null $type
+     * @param string|null $translate
+     * @param int         $visible
+     * @param array       $additionalData
+     *
+     * @throws \Exception
      */
-    public function insert($key, $value)
+    public function insert($key, $value, $group, $type, $translate = null, $visible = 1, array $additionalData = [])
     {
+        $this->context->beginTransaction();
         try
         {
-            $this->context->query('INSERT INTO setting VALUES (?, ?)', $key, $value);
+            $this->context->query('INSERT INTO setting', [
+                'Key'            => $key,
+                'Value'          => $value,
+                'Group'          => $group,
+                'Type'           => $type,
+                'Translate'      => $translate,
+                'Visible'        => $visible,
+                'AdditionalData' => json_encode($additionalData)
+            ]);
+            $this->context->commit();
         }
-        catch (UniqueConstraintViolationException $ex)
+        catch (\Exception $e)
         {
-            $this->updateValueByKey($key, $value);
+            $this->context->rollBack();
+            throw $e;
         }
     }
 
     public function updateValueByKey($key, $value)
+    /**
+     * @param string $column
+     * @param string $key
+     * @param string $value
+     *
+     * @throws \Exception
+     */
+    public function updateColumn($column, $key, $value)
     {
-        $this->context->query('UPDATE setting SET Value = ? WHERE Key = ?', $value, $key);
+        $this->context->beginTransaction();
+        try
+        {
+            $this->context->query('UPDATE setting SET ?name = ? WHERE Key = ?', $column, $value, $key);
+            $this->context->commit();
+        }
+        catch (\Exception $e)
+        {
+            $this->context->rollBack();
+            throw $e;
+        }
     }
 
 }
